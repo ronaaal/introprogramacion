ab7a61b2cb0974e0892a0faacae45fa7d5830df7
ronaaal <116390077+ronaaal@users.noreply.github.com>
2022-12-02 13:31:23 -0500

    Update Virtualizacion.java

--- src/Virtualizacion.java
+++ src/Virtualizacion.java
@@ -1,6 +1,8 @@
 public class Virtualizacion {
     public static void main(String[] args) {
         System.out.println("TEMAS DE PROYECTOS EXPLICADOS EN CENTRO DE CONVENCIONES");
-        System.out.println("Heyy muy buenas a todos guapísimos, aquí vegeta777 en una gameplay directo del planeta ");
+        System.out.println("Proyecto de la Gallina Turuleka, un videojuego programado en base al juego de Subway Surfers.");
+        System.out.println("Diseño de personajes para un juego didáctico de la Utpl para niños y adolescentes para fomentar el desarrollo de los conocimientos.");
+        System.out.println("Sala de clases virtual de la universidad Utpl.");
     }
 }
