ee32e4d0c30d2c8f7dfc62e81522ed6519add787
ronaaal <116390077+ronaaal@users.noreply.github.com>
2022-12-06 16:25:33 -0500

    Add files via upload

---
+++ src/semana1/NumeroAbundante.java
@@ -0,0 +1,25 @@
+package Bimestre2.semana1;
+
+import java.util.Scanner;
+
+public class NumeroAbundante {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        int contador = 1;
+        int suma_divisores = 0;
+        System.out.println("Ingrese el número: ");
+        int numero = teclado.nextInt();
+        while (contador < numero){
+            if (numero % contador == 0){
+                suma_divisores = suma_divisores + contador;
+            }
+            contador = contador + 1;
+        }
+        if (suma_divisores > numero){
+            System.out.println("Sí es abundante.");
+
+        } else {
+            System.out.println("No es abundante.");
+        }
+    }
+}
